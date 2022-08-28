8ddc92ce483966135989a5cbf8a0baa8a3e25bd3
Carl-Zoe <987906615@qq.com>
2022-08-28 11:46:52 +0800

    v1.2 添加geo_api_info获取失败的情况，未完全解决

--- clockin.py
+++ clockin.py
@@ -9,8 +9,13 @@ import random
 from utils import take_out_json
 from exceptions import *
 
-logging.basicConfig(level=logging.INFO)
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler()
+fmt = logging.Formatter(fmt = "%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s", datefmt="[%Y-%m-%d %X]")
+sh.setFormatter(fmt) 
+logger.addHandler(sh)
 
 url_login = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
 url_base = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
@@ -40,11 +45,11 @@ class ZJULogin:
         """
         check if the login page is accessible
         """
-        logging.info("connect to the url page...")
+        logger.info("connect to the url page...")
         resp = self.session.get(self.url_login)
 
         if resp.status_code == 200:
-            logging.info("successful 200 OK")
+            logger.info("successful 200 OK")
         else:
             raise LoginException("Fail to open Login Page, Check your Internet connection\n")
 
@@ -71,7 +76,7 @@ class ZJULogin:
         """
         login to ZJU platform
         """
-        logging.info("login...")
+        logger.info("login...")
         resp = self.session.get(self.url_login)
         execution = re.search('name="execution" value="(.*?)"', resp.text).group(1)
         # encrypt the password
@@ -89,7 +94,7 @@ class ZJULogin:
         if "统一身份认证" in resp.content.decode():
             raise LoginException("username or password error")
         else:
-            logging.info("login successful")
+            logger.info("login successful")
             return self.session
 
     def logout(self):
@@ -99,9 +104,9 @@ class ZJULogin:
         url_logout = "https://zjuam.zju.edu.cn/zjuam-main/security/logout"
         expire = self.session.get(url_logout).headers.get('Set-Cookie')
         if "Expires=Thu, 01-Jan-1970 00:00:00 GMT" in expire:
-            logging.info("logout successful")
+            logger.info("logout successful")
         else:
-            logging.error("logout failed")
+            logger.error("logout failed")
         
 
 class HealthCheckInHelper(ZJULogin):
@@ -129,18 +134,22 @@ class HealthCheckInHelper(ZJULogin):
             raise GetInfoException("Fail to get old info: index error")
 
         new_info = old_info.copy()
-
+        # print(new_info)
         # random position
-        geo_api_info = json.loads(old_info["geo_api_info"])
-        Q = geo_api_info["position"]["Q"] + random.random() * 0.00001
-        R = geo_api_info["position"]["R"] + random.random() * 0.00001
-        lng = geo_api_info["position"]["lng"] + random.random() * 0.00001
-        lat = geo_api_info["position"]["lat"] + random.random() * 0.00001
-        geo_api_info["position"]["Q"] = Q
-        geo_api_info["position"]["R"] = R
-        geo_api_info["position"]["lng"] = lng
-        geo_api_info["position"]["lat"] = lat
-        new_info["geo_api_info"] = geo_api_info
+        try:
+            geo_api_info = json.loads(old_info["geo_api_info"])
+            Q = geo_api_info["position"]["Q"] + random.random() * 0.00001
+            R = geo_api_info["position"]["R"] + random.random() * 0.00001
+            lng = geo_api_info["position"]["lng"] + random.random() * 0.00001
+            lat = geo_api_info["position"]["lat"] + random.random() * 0.00001
+            geo_api_info["position"]["Q"] = Q
+            geo_api_info["position"]["R"] = R
+            geo_api_info["position"]["lng"] = lng
+            geo_api_info["position"]["lat"] = lat
+            new_info["geo_api_info"] = geo_api_info
+        except json.JSONDecodeError as e:
+            raise GetInfoException("Fail to get old info: json decode error")
+
         # other information
         new_info['id'] = new_id
         new_info['name'] = name
